69e655a7c2a4d71e64e490ba4ae380d4a933c883
Jason Walstad <Jason.Walstad@experian.com>
2017-08-14 10:25:59 -0400

    remove default api route

--- civilopedia/App_Start/WebApiConfig.cs
+++ civilopedia/App_Start/WebApiConfig.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 
 namespace civilopedia
 {
@@ -13,12 +10,6 @@ namespace civilopedia
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
 
             config.Routes.MapHttpRoute(
                 name: "Civilizations",
